5cba3fb72de2a8bbf5cc0ea86d26ee1c6674f3f2
Stuart Maitland <stuartbman@hotmail.co.uk>
2022-01-14 17:50:35 +0000

    Changed analysis and improved plotting

--- plot_sentiment.py
+++ plot_sentiment.py
@@ -1,21 +1,22 @@
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from datetime import datetime as dt
+import matplotlib.dates as mdates
+import datetime as dt
 import numpy as np
 import networkx as nx
 
 mpl.use('macosx')
 duration = 60
 
-df = pd.read_csv('test6.csv')
+df = pd.read_csv('2017-end2021.csv')
 df['created_utc'] = pd.to_datetime(df['created_utc'], unit='s')
 df = df.set_index('created_utc')
 
 print('Dataframe length: {}'.format(len(df)))
 print('Number of users: {}'.format(df['author'].nunique()))
 
-# df = df[df['sentiment'] != 0] #Uncomment to remove zero elements (ie without sentiment)
+# df = df.drop(df[(df.sentiment < 0.05) & (df.sentiment > -0.05)].index)  # Uncomment to remove zero elements (ie without sentiment)
 
 df2 = df.resample("1d").mean().rolling(window=duration, min_periods=10, win_type='hanning').mean()
 df2['sentiment_sem'] = df.resample("1d")['sentiment'].mean().rolling(window=duration, min_periods=10).sem()
@@ -26,11 +27,22 @@ plt.fill_between(df2.index,
                  df2['sentiment'] - df2['sentiment_sem'],
                  df2['sentiment'] + df2['sentiment_sem'],
                  alpha=0.5, )
-plt.ylabel('Sentiment')
+plt.ylabel('Sentiment', fontsize=18)
+plt.yticks(np.arange(0, 0.5, 0.1))
+plt.tick_params(axis='both', which='major', labelsize=18)
 plt.twinx()
 plt.bar(df2.index, df2['count'], alpha=0.2, color='k')
-plt.ylabel('Daily comment count')
+plt.ylabel('Daily comment count', fontsize=18)
 
+ax = plt.gca()
+ax.xaxis.set_major_formatter(
+    mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
+ax.set_xlim([dt.date(2018, 3, 30), dt.date(2021, 12, 31)])
+ax.tick_params(axis='both', which='major', labelsize=18)
+
+fig = plt.gcf()
+fig.set_size_inches(12, 8)
+fig.savefig('test.pdf', dpi=300)
 plt.show()
 
 plt.boxplot(df['sentiment'])
@@ -60,6 +72,4 @@ def user_rank(df, username):
     print('sentiment: {:.2f}'.format(df.loc[df['author'] == username]['sentiment'].mean()))
     a = df['author'].value_counts()
     print('number comments: {}'.format(a[username]))
-    print('rank: {}'.format(np.where(a.values ==a[username])[0][0]+1))
-
-
+    print('rank: {}'.format(np.where(a.values == a[username])[0][0] + 1))
